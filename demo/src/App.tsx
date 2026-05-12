a2acdcb93fbe62e120200d632097adb638a432d4
Yeeun <yechukim217@gmail.com>
2026-05-12 18:17:02 +0900

    chore: resolve demo path

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,19 @@
-import { Smear } from 'smear'
+import { Smear } from "smear"
 
 export default function App() {
-    return <Smear/>
+  return (
+    <div
+      style={{
+        padding: 10,
+        borderRadius: 10,
+        background: "pink",
+        fontSize: 20,
+      }}
+    >
+      test
+      <Smear>
+        <div>test</div>
+      </Smear>
+    </div>
+  )
 }
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "node:path"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      smear: resolve(__dirname, "../src/index.ts"),
+    },
+  },
 })
