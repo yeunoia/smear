16ee55a088f411ec9db2c0ee6c7399ecba24de5a
Yeeun <yechukim217@gmail.com>
2026-05-14 20:08:58 +0900

    chore: config demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { resolve } from "node:path"
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias:
+      process.env.NODE_ENV === "production"
+        ? {}
+        : { "@yeunoia/smear": resolve(__dirname, "../src/index.ts") },
+  },
 })
