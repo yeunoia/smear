e10fde0fe2c17d462ae6fda34002920dbf26e933
Yeeun <yechukim217@gmail.com>
2026-05-14 20:02:35 +0900

    chore:remove unused

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { resolve } from "node:path"
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@yeunoia/smear": resolve(__dirname, "../src/index.ts"),
-    },
-  },
 })
